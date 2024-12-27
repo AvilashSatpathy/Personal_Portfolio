afd93624616b9fd4cdd6e9cdce3425f0e08b96aa
Avilash Satpathy <satpathyavilash@gmail.com>
2024-12-27 06:07:30 +0530

    Initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $(".about-info-inner").typed({
+      strings: [
+        "<span>Hi, I am Avilash Satpathy</span> I am a dedicated professional specializing in Data Science, Machine Learning, and Artificial Intelligence. With a strong foundation in designing and deploying predictive models, I excel at transforming complex datasets into actionable insights. My expertise includes developing scalable algorithms, optimizing performance, and delivering data-driven solutions to address real-world challenges. I am passionate about staying updated with the latest advancements in AI and leveraging them to create meaningful impact in diverse domains.",
+      ],
+      typeSpeed: 7,
+      contentType: "html",
+    });
+  }, 100);
